fbe50641593bf837accc7d8bebc990ababc05f44
rahul <degrarahul3@gmail.com>
2025-01-23 16:23:19 +0530

    task2-complete  two type of functionality

--- lib/movie_api.rb
+++ lib/movie_api.rb
@@ -1,7 +1,11 @@
 ## load httparty and dotenv
+require 'httparty'
+require 'dotenv'
+
+Dotenv.load
 
 class MovieAPI
-  base_uri ''
+  BASE_URL = 'https://www.omdbapi.com'
 
   def initialize
     @api_key = ENV['OMDB_API_KEY']
@@ -10,11 +14,15 @@ class MovieAPI
   # Search by movie title
   def search_by_title(title, options = {})
     ## write logic here to call omdb api
+    response = HTTParty.get("#{BASE_URL}/?apikey=#{@api_key}&s=#{title}")
+    parse_response(response)
   end
 
   # Search by IMDB ID
   def search_by_imdb_id(imdb_id, options = {})
     ## write logic here to call omdb api
+    response = HTTParty.get("#{BASE_URL}/?apikey=#{@api_key}&i=#{imdb_id}")
+    parse_response(response)
   end
 
   
@@ -23,5 +31,11 @@ class MovieAPI
 
   def parse_response(response)
     # parse raw api responsde data
+    if response.code == 200
+        response.parsed_response
+      else
+        puts "HTTP Error: #{response.code}"
+        nil
+      end
   end
 end 

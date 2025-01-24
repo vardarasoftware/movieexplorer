a03925fc2024edbbefe7772327607f3bec0e7038
Himanshi Sharma <himanshiksharma@outlook.com>
2025-01-24 12:24:17 +0530

    Save and show data into database (#6)
    * Get movie data from OMDB_API
    
    * Show data in script file
    
    * Establish db connection
    
    * Store movie data into database table
    
    * Display data in table format

---
+++ lib/movie_display.rb
@@ -0,0 +1,36 @@
+require 'table_print'
+require_relative 'movie_database'
+
+class MovieDisplay
+  def initialize
+    MovieDatabase.establish_connection
+  end
+
+  def display_all_movies
+    movies = Movie.all
+    tp movies, :id, :title, :year, :imdb_id, :poster, :created_at
+  end
+
+  def display_movie_by_title(title)
+    movie = Movie.find_by(title: title)
+    if movie
+      tp [movie], :id, :title, :year, :imdb_id, :poster, :created_at
+    else
+      puts "No movie found with title: #{title}"
+    end
+  end
+
+  def display_movie_by_imdb_id(imdb_id)
+    movie = Movie.find_by(imdb_id: imdb_id)
+    if movie
+      tp [movie], :id, :title, :year, :imdb_id, :poster, :created_at
+    else
+      puts "No movie found with IMDB ID: #{imdb_id}"
+    end
+  end
+
+  def display_recent_movies(limit = 5)
+    movies = Movie.order(created_at: :desc).limit(limit)
+    tp movies, :id, :title, :year, :imdb_id, :poster, :created_at
+  end
+end

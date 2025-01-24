92861182e447a467b06258578db2c00e01ffeb52
Himanshi Sharma <himanshiksharma@outlook.com>
2025-01-24 12:20:51 +0530

    Add movie search functionality (#4)
    * Get movie data from OMDB_API
    
    * Show data in script file

--- lib/movie_explorer.rb
+++ lib/movie_explorer.rb
@@ -11,19 +11,35 @@ class MovieExplorer
     # Searches for a movie by title using the MovieAPI
     # Saves the movie to the database if found
     # Returns the movie data or nil if no movie is found
-    nil
+    movie = @api.search_by_title(title)
+    if valid_result?(movie)
+      movie
+    else
+      nil
+    end
   end
 
   def search_and_save_by_imdb_id(imdb_id)
     # Searches for a movie by its IMDB ID using the MovieAPI
-    # 
+    #
     # @param imdb_id [String] The unique IMDB identifier for the movie
     # @return [Hash, nil] The movie data if found, or nil if no movie is found
-    # 
+    #
     # This method:
     # 1. Queries the MovieAPI for a movie using the provided IMDB ID
     # 2. If a movie is found, saves it to the database
     # 3. Returns the movie data or nil
+    movie = @api.search_by_imdb_id(imdb_id)
+    if valid_result?(movie)
+      movie
+    else
+      nil
+    end
   end
 
-end 
+  private
+
+  def valid_result?(result)
+    result.is_a?(Hash) && result['Response'] == 'True'
+  end
+end

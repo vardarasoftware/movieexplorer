1548013576bcb92daeb4519c79fe5604e1af5449
Himanshi Sharma <himanshiksharma@outlook.com>
2025-01-24 12:24:17 +0530

    Save and show data into database (#6)
    * Get movie data from OMDB_API
    
    * Show data in script file
    
    * Establish db connection
    
    * Store movie data into database table
    
    * Display data in table format

--- models/movie.rb
+++ models/movie.rb
@@ -1,15 +1,16 @@
-  # Schema for the movies table
-  # Columns:
-  # - title: string (movie title)
-  # - year: string (year of release)
-  # - imdb_id: string (unique IMDB identifier)
-  # - poster: string (URL or path to movie poster)
-  # - created_at: timestamp (when record was created)
-  # - updated_at: timestamp (when record was last updated)
+require 'active_record'
+# Schema for the movies table
+# Columns:
+# - title: string (movie title)
+# - year: string (year of release)
+# - imdb_id: string (unique IMDB identifier)
+# - poster: string (URL or path to movie poster)
+# - created_at: timestamp (when record was created)
+# - updated_at: timestamp (when record was last updated)
 
 class Movie < ActiveRecord::Base
   ## this is the model for the movie table
-  ## in order to interact with the database, you need to create a migration file    
+  ## in order to interact with the database, you need to create a migration file
   ## and then create a model file
   ## the migration file is located in the db/migrate folder
   ## the model file is located in the app/models folder

7a7fd298782c8df63f81a9127ecd39f8a97e159b
rahul <91818240+rahuldegra3@users.noreply.github.com>
2025-01-27 13:12:47 +0530

    task-4 create new columns in  movies  table (#8)

--- lib/show_table.rb
+++ lib/show_table.rb
@@ -6,7 +6,32 @@ class TableDisplay
   # Fetches all movies and displays them in a table format
   def self.display_searched_movies(movies)
     if movies.any?
-      tp movies.map { |movie| { title: movie['Title'], year: movie['Year'], imdb_id: movie['imdbID'], poster: movie['Poster'] } }
+      tp movies.map { |movie| { 
+        title: movie['Title'], 
+        year: movie['Year'], 
+        imdb_id: movie['imdbID'], 
+        poster: movie['Poster'], 
+        rated: movie['Rated'],
+        released: movie['Released'],
+        runtime: movie['Runtime'],
+        genre: movie['Genre'],
+        director: movie['Director'],
+        writer: movie['Writer'],
+        actors: movie['Actors'],
+        plot: movie['Plot'],
+        language: movie['Language'],
+        country: movie['Country'],
+        awards: movie['Awards'],
+        ratings: movie['Ratings'],
+        metascore: movie['Metascore'],
+        imdb_rating: movie['imdbRating'],
+        imdb_votes: movie['imdbVotes'],
+        movie_type: movie['Type'],
+        dvd: movie['DVD'],
+        box_office: movie['BoxOffice'],
+        production: movie['Production'],
+        website: movie['Website']
+      } }
     else
       puts "No movies found in the search results."
     end

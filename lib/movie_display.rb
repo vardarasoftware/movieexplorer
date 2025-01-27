93aff34198a33579af92fdbfa2786299ca18795e
heymanshe <himanshiksharma@outlook.com>
2025-01-27 13:29:35 +0530

    #task4

--- lib/movie_display.rb
+++ lib/movie_display.rb
@@ -8,13 +8,13 @@ class MovieDisplay
 
   def display_all_movies
     movies = Movie.all
-    tp movies, :id, :title, :year, :imdb_id, :poster, :created_at
+    tp movies, :id, :title, :year, :imdb_id, :poster, :genre, :director, :language, :writer, :runtime, :created_at
   end
 
   def display_movie_by_title(title)
     movie = Movie.find_by(title: title)
     if movie
-      tp [movie], :id, :title, :year, :imdb_id, :poster, :created_at
+      tp [movie], :id, :title, :year, :imdb_id, :poster, :genre, :director, :language, :writer, :runtime, :created_at
     else
       puts "No movie found with title: #{title}"
     end
@@ -23,7 +23,7 @@ class MovieDisplay
   def display_movie_by_imdb_id(imdb_id)
     movie = Movie.find_by(imdb_id: imdb_id)
     if movie
-      tp [movie], :id, :title, :year, :imdb_id, :poster, :created_at
+      tp [movie], :id, :title, :year, :imdb_id, :poster, :genre, :director, :language, :writer, :runtime, :created_at
     else
       puts "No movie found with IMDB ID: #{imdb_id}"
     end
@@ -31,6 +31,6 @@ class MovieDisplay
 
   def display_recent_movies(limit = 5)
     movies = Movie.order(created_at: :desc).limit(limit)
-    tp movies, :id, :title, :year, :imdb_id, :poster, :created_at
+    tp movies, :id, :title, :year, :imdb_id, :poster, :genre, :director, :language, :writer, :runtime, :created_at
   end
 end
